6748e8797dcf6eec6e1a4d71141f410f9ee8f90a
Alex Xu <ax20146@my.westlake.school.nz>
2024-03-19 16:12:28 +1300

    Add: Tileset for map

---
+++ maps/Tilesets/Tiles.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tiles" tilewidth="64" tileheight="64" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../../assets/Tiles/Grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../../assets/Tiles/PathHorizontal.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../../assets/Tiles/PathVertical.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../../assets/Tiles/PathCornerTL.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../../assets/Tiles/PathCornerTR.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../assets/Tiles/PathCornerBL.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../../assets/Tiles/PathCornerBR.png"/>
+ </tile>
+</tileset>
