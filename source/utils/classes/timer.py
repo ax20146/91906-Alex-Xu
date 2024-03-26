a0a58c82e9fc2b089746eea335cadaf1b7c0ee64
Alex Xu <ax20146@my.westlake.school.nz>
2024-03-27 12:08:48 +1300

    Revert "Add: Comments to timer.py"
    This reverts commit e6ae4388f9e24b051b74e0c3cd3c272ecad04c88.

--- source/utils/classes/timer.py
+++ source/utils/classes/timer.py
@@ -7,32 +7,14 @@ from .clock import Clock
 
 # Define Timer Class
 class Timer:
-    """`Timer` class that represent a duration timer for cooldown"""
-
     __slots__ = "delay", "_clock", "_previous"
 
     def __init__(self, clock: Clock, delay: int) -> None:
-        """Initialise a `timer` object
-
-        Args:
-            clock (Clock): The clock used for time tracking
-            delay (int): The duration to time (0 to disable timing)
-        """
-
-        # Define public attributes
         self.delay: int = delay
-
-        # Define private attributes
         self._clock: Clock = clock
         self._previous: float = clock.time
 
     def available(self) -> bool:
-        """Get whether the duration being timed is over
-
-        Returns:
-            bool: The result of whether duration is over
-        """
-
         return (
             self._clock.time - self._previous >= self.delay
             if self.delay > 0
@@ -40,6 +22,4 @@ class Timer:
         )
 
     def update(self) -> None:
-        """Update the timer to start re-timing"""
-
         self._previous = self._clock.time
