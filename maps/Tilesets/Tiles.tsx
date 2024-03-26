03db860590acd050d9745a22cb791de27b322f23
Alex Xu <ax20146@my.westlake.school.nz>
2024-03-26 19:15:01 +1300

    Add: Easy Map

---
+++ maps/Tilesets/Tiles.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tiles" tilewidth="64" tileheight="64" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="10">
+  <image width="64" height="64" source="../../assets/Tiles/InnerBR.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="../../assets/Tiles/InnerBL.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../../assets/Tiles/OuterTL.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../../assets/Tiles/OuterTR.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../../assets/Tiles/SideTop.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="../../assets/Tiles/InnerTR.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="../../assets/Tiles/InnerTL.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../../assets/Tiles/OuterBL.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../../assets/Tiles/OuterBR.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../../assets/Tiles/SideBottom.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../../assets/Tiles/SideLeft.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../assets/Tiles/SideRight.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../../assets/Tiles/Grass.png"/>
+ </tile>
+ <tile id="0">
+  <image width="64" height="64" source="../../assets/Entities/Slot.png"/>
+ </tile>
+</tileset>
