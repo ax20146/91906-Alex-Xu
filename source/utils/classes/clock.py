422be78890f2b702130ca30f324d68b46867428f
Alex Xu <ax20146@my.westlake.school.nz>
2024-03-27 12:08:20 +1300

    Revert "Add: Comments to clock class"
    This reverts commit 5a85c3d1928d92e599fed7d18e6d1cf43b47c3ea.

--- source/utils/classes/clock.py
+++ source/utils/classes/clock.py
@@ -3,32 +3,14 @@
 
 # Define Clock Class
 class Clock:
-    """`Clock` class that represent a ticking clock for time tracking"""
-
     __slots__ = "_time"
 
     def __init__(self) -> None:
-        """Initialise a `Clock` object"""
-
-        # Define private attribute
         self._time: float = 0
 
     @property
     def time(self) -> float:
-        """Get current value of time attribute
-
-        Returns:
-            float: The current time value (in milliseconds)
-        """
-
         return self._time
 
     def update(self, delta_time: float) -> None:
-        """Update the clock with a change in time
-
-        Args:
-            delta_time (float): The change in time since last tick
-        """
-
-        # Update by 'delta_time' in milliseconds
         self._time += delta_time * 1000
