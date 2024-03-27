7718d1bcade980d84cded18724b4547369e2de28
Alex Xu <ax20146@my.westlake.school.nz>
2024-03-27 17:02:17 +1300

    Update: Easy map

--- source/entities/coins.py
+++ source/entities/coins.py
@@ -1,10 +1,10 @@
 # /entities/coins.py
 
 
-from ..utils import Movement, Sprite, Timer, Vector
+from ..utils import Sprite, Timer, Vector
 from ..utils.constants import TILE_SIZE
-from ..utils.functions import limit_within, randrange
-from ..utils.types import ClassVar, final
+from ..utils.functions import limit_within
+from ..utils.types import final
 
 __all__: list[str] = [
     "Coin",
@@ -15,42 +15,24 @@ __all__: list[str] = [
 
 
 class Coin(Sprite):
-    COLLECTION_RANGE = TILE_SIZE * 3
     LIFETIME = 8000
-    SPEED = 25
 
-    FILENAME: ClassVar[str]
-    VALUE: ClassVar[int]
+    FILENAME: str
+    VALUE: int
 
-    def __init__(
-        self,
-        position: Vector,
-    ) -> None:
-        position = Vector(
-            randrange(position.x, TILE_SIZE),
-            randrange(position.y, TILE_SIZE),
-        )
-
+    def __init__(self, position: Vector) -> None:
         super().__init__(
             filename=self.FILENAME,
-            position=limit_within(position),
+            position=limit_within(position.randomise(TILE_SIZE)),
         )
 
-        self.timer: Timer = Timer(self.clock, delay=self.LIFETIME)
-        self.movement: Movement = Movement(self, self.SPEED)
+        self.timer: Timer = Timer(self.clock, self.LIFETIME)
 
-    def attract(self, target: Vector) -> None:
-        if self.position.within(target, radius=self.COLLECTION_RANGE):
-            self.movement.update_target(target)
-
-    def collect(self) -> int:
+    def on_collect(self) -> int:
         self.kill()
         return self.VALUE
 
     def on_update(self, delta_time: float) -> None:
-        if self.movement.has_target():
-            self.movement.move(delta_time)
-
         if self.timer.available():
             self.kill()
 
@@ -58,7 +40,7 @@ class Coin(Sprite):
 @final
 class Gold(Coin):
     FILENAME = "./assets/Entities/Coins/Gold.png"
-    VALUE = 10
+    VALUE = 15
 
 
 @final
