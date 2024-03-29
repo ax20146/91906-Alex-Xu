da087b68c01e8f6e65c1deacad1afafb6c375fe2
Alex Xu <ax20146@my.westlake.school.nz>
2024-03-29 20:39:52 +1300

    Update: Maps with decorations

---
+++ maps/Tilesets/Decorations.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Decorations" tilewidth="64" tileheight="64" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../../assets/Decorations/BushBig.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../../assets/Decorations/BushSmall.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../../assets/Decorations/GrassBig.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../../assets/Decorations/GrassSmall.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../../assets/Decorations/RockBig.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../assets/Decorations/RockMedium.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../../assets/Decorations/RockSmall.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../../assets/Decorations/CraterBig.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../../assets/Decorations/CraterSmall.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../../assets/Decorations/Dust.png"/>
+ </tile>
+</tileset>
